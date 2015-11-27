6e2bc9aa2b215af8ef2e72b3e65b006eee25547d
Roland Zeng <rzeng@g.ucla.edu>
2015-11-26 17:35:29 -0800

    initial commit

---
+++ Assets/My Scripts/Player Scripts/PlayerBullet.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBullet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter(Collider other) {
+		
+		if (other.gameObject.tag == "Enemy") {
+			print ("enemy tag");
+			GameObject target = other.gameObject;
+			target.SendMessage("decrementEnemyHealth", SendMessageOptions.DontRequireReceiver);
+			
+			Destroy(gameObject);
+		}
+			
+	}
+}
